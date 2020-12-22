6d2ec2802fdb6512ba260c42249fd3567127cecb
bo <bogdan.fedorchenko@skf.com>
2020-12-22 09:57:34 +0200

    -- tests

--- test/AppUserControllerSpec.scala
+++ test/AppUserControllerSpec.scala
@@ -3,7 +3,7 @@ import com.github.storytime.dao.AppUserDao
 import com.github.storytime.model.AppUser
 import org.mockito.MockitoSugar
 import org.scalatestplus.play._
-import play.api.http.Status.OK
+import play.api.http.Status.{NOT_FOUND, OK}
 import play.api.test.Helpers.{contentAsJson, defaultAwaitTimeout, status}
 import play.api.test.{FakeRequest, Helpers}
 
@@ -12,26 +12,29 @@ import scala.concurrent.Future
 
 class AppUserControllerSpec extends PlaySpec with MockitoSugar {
 
-  private val emptyList = Future(Seq.empty)
-  private val notEmptyList = Seq(
-    AppUser(None, "", "", 1L),
-    AppUser(Some(1L), "", "", 1L),
-    AppUser(None, "qeFHFmo5yEU8NJCY", "ibXBWtDZoSEaSAJ1", 1L),
-    AppUser(Some(1L), "qeFHFmo5yEU8NJCY", "ibXBWtDZoSEaSAJ1", 1L),
-    AppUser(None, "cMvE7xM0fYWsbPzA", "drD4qCzMOfWbhqIT", 1L, ""),
-    AppUser(Some(1L), "7UoeYmUgGfK6jx02", "x75OjmQnpCbqrMGo", 1L, "qJMLSmHEwpfaBjVx"),
-    AppUser(None, "7UoeYmUgGfK6jx02", "x75OjmQnpCbqrMGo", 1L, "qJMLSmHEwpfaBjVx", ynabSyncEnabled = true),
-    AppUser(Some(1L), "7UoeYmUgGfK6jx02", "x75OjmQnpCbqrMGo", 1L, "qJMLSmHEwpfaBjVx", ynabSyncEnabled = true)
+  private val mockedAppUsersDao = mock[AppUserDao]
+  private val USER_ID = 1L
+  private val ZEN_TIME = 1608623328L
+
+  private val usersList = Seq(
+    AppUser(None, "", "", USER_ID),
+    AppUser(Some(USER_ID), "", "", USER_ID),
+    AppUser(None, "qeFHFmo5yEU8NJCY", "ibXBWtDZoSEaSAJ1", ZEN_TIME),
+    AppUser(Some(USER_ID), "qeFHFmo5yEU8NJCY", "ibXBWtDZoSEaSAJ1", ZEN_TIME),
+    AppUser(None, "cMvE7xM0fYWsbPzA", "drD4qCzMOfWbhqIT", ZEN_TIME, ""),
+    AppUser(Some(USER_ID), "7UoeYmUgGfK6jx02", "x75OjmQnpCbqrMGo", ZEN_TIME, "qJMLSmHEwpfaBjVx"),
+    AppUser(None, "7UoeYmUgGfK6jx02", "x75OjmQnpCbqrMGo", ZEN_TIME, "qJMLSmHEwpfaBjVx", ynabSyncEnabled = true),
+    AppUser(Some(USER_ID), "7UoeYmUgGfK6jx02", "x75OjmQnpCbqrMGo", ZEN_TIME, "qJMLSmHEwpfaBjVx", ynabSyncEnabled = true)
   )
+  private val notValidUsersList = Seq(AppUser(None, null, null, ZEN_TIME))
+  private val validUser = AppUser(Some(USER_ID), "7UoeYmUgGfK6jx02", "x75OjmQnpCbqrMGo", USER_ID, "qJMLSmHEwpfaBjVx", ynabSyncEnabled = true)
+  private val notValidUser = AppUser(None, null, null, USER_ID)
 
   "AppUserController" should {
     "1. getAllAppUsers can return empty list" in {
-      val mockedAppUsersDao = mock[AppUserDao]
+      when(mockedAppUsersDao.getAllAppUsers).thenReturn(Future(Seq.empty))
 
-      when(mockedAppUsersDao.getAllAppUsers).thenReturn(emptyList)
-
-      val controller = new AppUserController(mockedAppUsersDao, Helpers.stubControllerComponents())
-      val response = controller.getAllAppUsers().apply(FakeRequest())
+      val response = getController.getAllAppUsers().apply(FakeRequest())
 
       status(response) mustBe OK
       val body = contentAsJson(response).as[Seq[AppUser]]
@@ -40,31 +43,47 @@ class AppUserControllerSpec extends PlaySpec with MockitoSugar {
     }
 
     "2. getAllAppUsers can return full list" in {
-      val mockedAppUsersDao = mock[AppUserDao]
-      val mockedNotEmptyUsersList = Future(notEmptyList)
+      val mockedNotEmptyUsersList = Future(usersList)
 
       when(mockedAppUsersDao.getAllAppUsers).thenReturn(mockedNotEmptyUsersList)
-
-      val controller = new AppUserController(mockedAppUsersDao, Helpers.stubControllerComponents())
-      val response = controller.getAllAppUsers().apply(FakeRequest())
+      val response = getController.getAllAppUsers().apply(FakeRequest())
 
       status(response) mustBe OK
       val body = contentAsJson(response).as[Seq[AppUser]]
-      body.length mustBe notEmptyList.length
-      body.toSeq.sortBy(_.id) mustBe notEmptyList.sortBy(_.id)
+      body.length mustBe usersList.length
+      body.sortBy(_.id) mustBe usersList.sortBy(_.id)
     }
 
     "3. getAllAppUsers should not map empty body" in {
-      val mockedAppUsersDao = mock[AppUserDao]
-      val mockedNotEmptyUsersList = Future(Seq(AppUser(None, null, null, 1L)))
-      when(mockedAppUsersDao.getAllAppUsers).thenReturn(mockedNotEmptyUsersList)
-
-      val controller = new AppUserController(mockedAppUsersDao, Helpers.stubControllerComponents())
-      val response = controller.getAllAppUsers().apply(FakeRequest())
+      when(mockedAppUsersDao.getAllAppUsers).thenReturn(Future(notValidUsersList))
+      val response = getController.getAllAppUsers().apply(FakeRequest())
 
       status(response) mustBe OK
       intercept[Exception](contentAsJson(response).as[Seq[AppUser]])
     }
 
+    "4. getAppUserById must return valid user" in {
+      when(mockedAppUsersDao.getAppUserById(USER_ID)).thenReturn(Future(Some(validUser)))
+      val response = getController.getAppUserById(USER_ID).apply(FakeRequest())
+
+      status(response) mustBe OK
+      val body = contentAsJson(response).as[AppUser]
+      body mustBe validUser
+    }
+
+    "5. getAppUserById can return not found" in {
+      when(mockedAppUsersDao.getAppUserById(USER_ID)).thenReturn(Future(None))
+      val response = getController.getAppUserById(USER_ID).apply(FakeRequest())
+      status(response) mustBe NOT_FOUND
+    }
+
+    "6. getAppUserById can return not valid body" in {
+      when(mockedAppUsersDao.getAppUserById(USER_ID)).thenReturn(Future(Some(notValidUser)));
+      val response = getController.getAppUserById(USER_ID).apply(FakeRequest())
+      status(response) mustBe OK
+      intercept[Exception](contentAsJson(response).as[AppUser])
+    }
   }
+
+  private def getController = new AppUserController(mockedAppUsersDao, Helpers.stubControllerComponents())
 }
